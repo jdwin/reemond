cb9e2a2fa39691acfeaa6e99b0c78a65e226b3e9
sarkio <sarkio@outlook.com>
2014-08-30 16:35:22 +0100

    initial commit

---
+++ js/menu.js
@@ -0,0 +1,28 @@
+$(document.ready(function() {
+    $('section').waypoint(function(direction) {
+        var activeSection = $(this);
+        if(direction ==='down') {
+            activeSection = $(this).next();
+	}
+	
+	var sectionId = activeSection.attr('id');
+	$('ul li').removeClass('active');
+	$('ul li.' + sectionId).addClass('active');
+	console.log(activeSection);
+    });
+});
+
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'')==this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+	target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	    if (target.length) {
+	        $('html, body').animate({
+		    scrollTop: target.offset().top - (target.height() / 5)
+	    }, 500);
+	    return false;
+       }
+    }
+});
+
