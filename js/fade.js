4c2d18fdacf0cd2e4bfcb6b782aa6a17c1599617
sarkio <sarkio@outlook.com>
2014-09-01 01:49:45 +0100

    deleted:    .index.html.swp 	deleted:    css/.main.css.swp 	modified:   css/main.css 	modified:   index.html 	new file:   js/fade.js

---
+++ js/fade.js
@@ -0,0 +1,17 @@
+$(function() { 
+    $(window).scroll(function () {
+        var scrollTop = $(window).scrollTop();
+        if(scrollTop != 0)
+            $('#menu').stop().animate({'opacity':'0'});
+        else
+            $('#menu').stop().animate({'opacity':'1'});
+    });
+
+    $(window).scroll(function () {
+        var scrollTop = $(window).scrollTop();
+        if(scrollTop != 0)
+            $('#menu2').stop().animate({'opacity':'1'});
+        else
+            $('#menu2').stop().animate({'opacity':'0'});
+    });
+});
