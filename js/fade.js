bd508e5423f85129073c42a97bf05ef3d2d082bb
sarkio <sarkio@outlook.com>
2014-09-03 22:29:41 +0100

    modified:   css/main.css 	modified:   index.html 	modified:   js/fade.js

--- js/fade.js
+++ js/fade.js
@@ -1,15 +1,15 @@
 $(function() { 
     $(window).scroll(function () {
         var scrollTop = $(window).scrollTop();
-        if(scrollTop != 0)
-            $('#menu').stop().animate({'opacity':'0'});
-        else
+        if(scrollTop < 400)
             $('#menu').stop().animate({'opacity':'1'});
+        else
+            $('#menu').stop().animate({'opacity':'0'});
     });
 
     $(window).scroll(function () {
         var scrollTop = $(window).scrollTop();
-        if(scrollTop >= 9)
+        if(scrollTop > 399)
             $('#menu2').stop().animate({'opacity':'1'});
         else
             $('#menu2').stop().animate({'opacity':'0'});
