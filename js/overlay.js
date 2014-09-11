136435970775c308dd32c2e1fa83715e7317b03a
sarkio <sarkio@outlook.com>
2014-09-11 10:46:53 +0100

    modified:   css/main.css 	modified:   css/response.css 	modified:   js/overlay.js

--- js/overlay.js
+++ js/overlay.js
@@ -4,78 +4,66 @@ $(document).ready(function() {
     $('#videoReel1').bind('mouseenter', function() {
         $('#videoReel1 img').animate({"opacity":0.1});
         $('#videoReel1 .overlay').animate({"opacity":1});
+        $(".overlay").width($("#videoReel1 img").width());
     });
     $('#videoReel1').bind('mouseleave', function() {
         $('#videoReel1 img').animate({"opacity":1});
         $('#videoReel1 .overlay').animate({"opacity":0.1});
-    });
-    $(function() {
-        $(".overlay").width($("#videoReel1 img").width());
     });
 
     // Reel Two ------------------------------------------
     $('#videoReel2').bind('mouseenter', function() {
         $('#videoReel2 img').animate({"opacity":0.1});
         $('#videoReel2 .overlay').animate({"opacity":1});
+        $(".overlay").width($("#videoReel2 img").width());
     });
     $('#videoReel2').bind('mouseleave', function() {
         $('#videoReel2 img').animate({"opacity":1});
         $('#videoReel2 .overlay').animate({"opacity":0});
-    });
-    $(function() {
-        $(".overlay").width($("#videoReel2 img").width());
     });
 
     // Reel Three -----------------------------------------
     $('#videoReel3').bind('mouseenter', function() {
         $('#videoReel3 img').animate({"opacity":0.1});
         $('#videoReel3 .overlay').animate({"opacity":1});
+        $(".overlay").width($("#videoReel3 img").width());
     });
     $('#videoReel3').bind('mouseleave', function() {
         $('#videoReel3 img').animate({"opacity":1});
         $('#videoReel3 .overlay').animate({"opacity":0});
-    });
-    $(function() {
-        $(".overlay").width($("#videoReel3 img").width());
     });
 
     // Reel Four -----------------------------------------
     $('#videoReel4').bind('mouseenter', function() {
         $('#videoReel4 img').animate({"opacity":0.1});
         $('#videoReel4 .overlay').animate({"opacity":1});
+        $(".overlay").width($("#videoReel4 img").width());
     });
     $('#videoReel4').bind('mouseleave', function() {
         $('#videoReel4 img').animate({"opacity":1});
         $('#videoReel4 .overlay').animate({"opacity":0});
-    });
-    $(function() {
-        $(".overlay").width($("#videoReel4 img").width());
     });
 
     // Reel Five -----------------------------------------
     $('#videoReel5').bind('mouseenter', function() {
         $('#videoReel5 img').animate({"opacity":0.1});
         $('#videoReel5 .overlay').animate({"opacity":1});
+        $(".overlay").width($("#videoReel5 img").width());
     });
     $('#videoReel5').bind('mouseleave', function() {
         $('#videoReel5 img').animate({"opacity":1});
         $('#videoReel5 .overlay').animate({"opacity":0});
-    });
-    $(function() {
-        $(".overlay").width($("#videoReel5 img").width());
     });
 
     // Reel Six ------------------------------------------
     $('#videoReel6').bind('mouseenter', function() {
         $('#videoReel6 img').animate({"opacity":0.3});
         $('#videoReel6 .overlay').animate({"opacity":1});
+        $(".overlay").width($("#videoReel6 img").width());
     });
     $('#videoReel6').bind('mouseleave', function() {
         $('#videoReel6 img').animate({"opacity":1});
         $('#videoReel6 .overlay').animate({"opacity":0});
-    });
-    $(function() {
-        $(".overlay").width($("#videoReel6 img").width());
     });
 
 });
