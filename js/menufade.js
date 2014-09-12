291b3209537f934c13f94c127932cd2d99f27323
sarkio <sarkio@outlook.com>
2014-09-12 16:29:39 +0100

    modified:   js/menufade.js

--- js/menufade.js
+++ js/menufade.js
@@ -4,26 +4,18 @@ $(function() {
     $(window).scroll(function () {
         var scrollTop = $(window).scrollTop();
         if(scrollTop < 230)
-            $('#menu').stop().fadeIn('fast');
+            $('#menu').fadeIn('fast');
         else
-            $('#menu').stop().fadeOut('fast');
+            $('#menu').fadeOut('fast');
     });
 
     //Fade in second menu
     $(window).scroll(function () {
         var scrollTop = $(window).scrollTop();
         if(scrollTop > 399)
-            $('#menu2').stop().animate({'opacity':'1'});
+            $('#menu2').animate({'opacity':'1'});
         else
-            $('#menu2').stop().animate({'opacity':'0'});
+            $('#menu2').animate({'opacity':'0'});
     });
 
-    //Fade in background for last section
-   // $(window).scroll(function () {
-   //     var scrollTop = $(window).scrollTop();
-   //     if(scrollTop < 850)
-   //         $('#d4 .ree').animate({'opacity':'0'});
-   //     else
-   //         $('#d4 .ree').animate({'opacity':'1'}, {'duration':'100'});
-   // });
 });
