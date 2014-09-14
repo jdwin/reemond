52017fa33ef07e1fffcfdbd625adec4d3bc12be0
sarkio <sarkio@outlook.com>
2014-09-14 17:12:11 +0100

    modified:   .index.html.un~ 	modified:   css/main.css 	deleted:    img/bbforsite.mp4 	deleted:    img/birdsforsite.mp4 	deleted:    img/city.gif 	deleted:    img/facebook-logo2.png 	modified:   index.html 	renamed:    js/main.js -> js/parallax.js 	new file:   js/video.js 	renamed:    img/bbforsitelarge.mp4 -> vid/birdbridge.mp4 	new file:   vid/torontozoom.mp4 	new file:   vid/torontozoomout.mp4 	new file:   vid/vancouverbridge.mp4 	new file:   vid/vancouverearth.mp4 	new file:   vid/vancouverglass.mp4

---
+++ js/video.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    var position = 1,
+        playlist = ['../vid/vancouverearth.mp4', '../vid/torontozoom.mp4', '../vid/vancouverglass.mp4'],
+        video = document.getElementById('splashVid');
+
+    video.addEventListener('ended', rotate_video, false);
+
+    function rotate_video() {
+        video.setAttribute('src', playlist[position]);
+        video.load();
+        position++;
+        if (position >= playlist.length) {position= 0;}
+    }
+});
