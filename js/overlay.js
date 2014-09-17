feed1ac1e296e3663b06eaab81f1ce052aed0e34
sarkio <sarkio@outlook.com>
2014-09-17 23:52:47 +0100

    modified:   css/main.css 	modified:   index.html 	modified:   js/overlay.js

--- js/overlay.js
+++ js/overlay.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     $('#videoReel1').bind('mouseenter', function() {
         $('#videoReel1 img').animate({"opacity":0.1});
         $('#videoReel1 .overlay').animate({"opacity":1});
+        $(".overlay").width($("#videoReel1 img").width());
     });
     $('#videoReel1').bind('mouseleave', function() {
         $('#videoReel1 img').animate({"opacity":1});
@@ -14,6 +15,7 @@ $(document).ready(function() {
     $('#videoReel2').bind('mouseenter', function() {
         $('#videoReel2 img').animate({"opacity":0.1});
         $('#videoReel2 .overlay').animate({"opacity":1});
+        $(".overlay").width($("#videoReel2 img").width());
     });
     $('#videoReel2').bind('mouseleave', function() {
         $('#videoReel2 img').animate({"opacity":1});
@@ -24,6 +26,7 @@ $(document).ready(function() {
     $('#videoReel3').bind('mouseenter', function() {
         $('#videoReel3 img').animate({"opacity":0.1});
         $('#videoReel3 .overlay').animate({"opacity":1});
+        $("#videoReel3 img").width($(".overlay").width());
     });
     $('#videoReel3').bind('mouseleave', function() {
         $('#videoReel3 img').animate({"opacity":1});
@@ -34,6 +37,7 @@ $(document).ready(function() {
     $('#videoReel4').bind('mouseenter', function() {
         $('#videoReel4 img').animate({"opacity":0.1});
         $('#videoReel4 .overlay').animate({"opacity":1});
+        $(".overlay").width($("#videoReel4 img").width());
     });
     $('#videoReel4').bind('mouseleave', function() {
         $('#videoReel4 img').animate({"opacity":1});
@@ -44,6 +48,7 @@ $(document).ready(function() {
     $('#videoReel5').bind('mouseenter', function() {
         $('#videoReel5 img').animate({"opacity":0.1});
         $('#videoReel5 .overlay').animate({"opacity":1});
+        $(".overlay").width($("#videoReel5 img").width());
     });
     $('#videoReel5').bind('mouseleave', function() {
         $('#videoReel5 img').animate({"opacity":1});
@@ -54,6 +59,7 @@ $(document).ready(function() {
     $('#videoReel6').bind('mouseenter', function() {
         $('#videoReel6 img').animate({"opacity":0.3});
         $('#videoReel6 .overlay').animate({"opacity":1});
+        $(".overlay").width($("#videoReel6 img").width());
     });
     $('#videoReel6').bind('mouseleave', function() {
         $('#videoReel6 img').animate({"opacity":1});
