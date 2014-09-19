9315df3dec991f64fe96e9c06d23a7f8b4ff69e6
sarkio <sarkio@outlook.com>
2014-09-19 13:14:02 +0100

    modified:   css/main.css 	modified:   css/response.css 	new file:   img/storm.jpg 	modified:   index.html 	modified:   js/mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -1,6 +1,16 @@
-$(window).resize(function(){
-        if($(window).width() < 961) {
-            $("#splashVid").remove();
-            $("#recentVid").remove();
-        }
+$(document).ready(function() {
+    noVid();
 });
+
+$(window).resize(function() {
+    noVid();
+});
+
+function noVid() {
+
+    console.log($(window).width());
+
+    if($(window).width() < 961) {
+        $(".noVid").remove();
+    }
+};
