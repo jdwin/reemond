0c1515cb50712fa08b304f75dba74e0f6579f47e
sarkio <sarkio@outlook.com>
2014-09-24 10:59:27 +0100

    modified:   js/main.js 	deleted:    js/mobile.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,44 @@
 $(document).ready(function() {
-
-    $window = $(window);
-    $section = $(".scrollView");
-
-    resizeWindow();
-
-    function resizeWindow() {
-
-        viewH = $window.height();
-        viewW = $window.width();
-
-        var s = skrollr.init({
-            forceHeight: false
-        });
-
-        $section.height(viewH);
-        $section.width(viewW);
-
-        s.refresh($(".scrollView"));
-    }
-
+    checkSize();
 });
+$(window).resize(function() {
+    checkSize();
+});
+
+$window = $(window);
+$section = $(".scrollView");
+
+function resizeWindow() {
+
+    viewH = $window.height();
+    viewW = $window.width();
+
+    var s = skrollr.init({
+        forceHeight: false
+    });
+
+    $section.height(viewH);
+    $section.width(viewW);
+
+    s.refresh($(".scrollView"));
+};
+
+function noMobile() {
+    $('.mobile').remove();
+};
+
+function checkSize() {
+
+    viewW = $window.width();
+
+    if (viewW > 961) {
+
+        resizeWindow();
+
+    } else {
+
+        noMobile();
+
+    };
+};
+
