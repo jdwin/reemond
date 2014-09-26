2f50f057655302338dd24cb4f62f921db5c42893
sarkio <sarkio@outlook.com>
2014-09-26 14:40:34 +0100

    modified:   css/mobile.css 	modified:   favicon.ico 	new file:   icon.png 	new file:   img/leftarrowwhite.png 	new file:   img/rightarrowwhite.png 	new file:   js/overlay3.js 	modified:   video.html

---
+++ js/overlay3.js
@@ -0,0 +1,25 @@
+function overlayAssign(target) {
+    $('videoReel' + target).bind('mouseenter', function() {
+            $('#videoReel' + target + ' img').animate({'opacity':0.1});
+            $('#videoReel' + target + ' .overlay').animate({'opacity':1});
+            $('.overlay').width($('#videoReel' + target + ' img').width());
+            $('.overlay').height($('#videoReel' + target + ' img').height());
+    });
+    $('videoReel' + target).bind('mouseleave', function() {
+        $('videoReel' + target + ' img').animate({'opacity':1});
+        $('videoReel' + target ' .overlay').animate({'opacity':0});
+    });
+};
+
+function overlayExecute() {
+
+    var targets = ['1','2','3','4','5','6'];
+
+    for (target in targets) {
+        overlayAssign(target);
+    };
+};
+
+$(document).ready(function() {
+    overlayExecute();
+});
